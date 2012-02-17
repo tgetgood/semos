c243fc0e9788f7dabb22b675efe528729f321aa1
Thomas Getgood <thomas.getgood@gmail.com>
2012-02-16 21:55:30 -0500

    Replace localhost with 127.0.0.1
    This is more compatible with the tomcat config.

--- servers/search/scripts/wipe_index.sh
+++ servers/search/scripts/wipe_index.sh
@@ -5,4 +5,4 @@
 # Only works from localhost (provided tomcat is configured properly). Use with
 # caution. 
 
-curl 'localhost:8080/solr/update/?commit=true' --data-binary '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml' 
+curl '127.0.0.1:8080/solr/update/?commit=true' --data-binary '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml' 
